394f5cb05632d966d35dd6cd75c3a4b0fdcac758
Gaurvi17 <95625191+Gaurvi17@users.noreply.github.com>
2024-01-24 21:56:57 +0530

    Update primeSqrtN.cpp

--- primeSqrtN.cpp
+++ primeSqrtN.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n,flag=0;
     cin>>n;
-    for(int i=2;i<n;i++)
+    for(int i=2;i<=sqrt(n);i++)
     {
         if(n%i==0)
         {
